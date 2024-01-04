3db2e500e8a54ec37aa1b000ce4693ab66b6b0dd
Marc Brigham <marc.brigham@gmail.com>
2024-01-04 10:09:07 -0500

    add vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import { ThemeProvider } from './components/theme-provider'
+import { Analytics } from '@vercel/analytics/react';
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 
@@ -29,6 +30,7 @@ export default function RootLayout({
           <Navbar />
           {children}
           <Footer />
+          <Analytics />
         </ThemeProvider>
         
       </body>
