7c6fbc2503ce6594ec8cc20c2c0bf573a7fce994
Marc Brigham <marc.brigham@gmail.com>
2024-01-18 21:37:54 -0500

    change footer in light mode

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,31 +1,33 @@
 import Image from "next/image";
 import Link from "next/link";
 
+import styles from "./footer.css";
+
 export default function Footer() {
-    return (
-        <footer className="border-t py-4 sticky bottom-0 bg-slate-950">
-            <div className="container mx-auto flex justify-center space-x-6">
-                <Link href="https://github.com/marcbrigham">
-                    <Image 
-                            src='/github.svg'
-                            width={30} 
-                            height={30} 
-                            alt="github"
-                            priority
-                            className="dark:invert"
-                        />
-                </Link>
-                <Link href="https://x.com/marcbrigham">
-                    <Image 
-                        src='/x.svg'
-                        width={30} 
-                        height={30} 
-                        alt="github"
-                        priority
-                        className="dark:invert"
-                    />
-                </Link>
-            </div>
-        </footer>
-    );
+  return (
+    <footer className="border-t py-4 sticky bottom-0 bg-white dark:bg-slate-950">
+      <div className="container mx-auto flex justify-center space-x-6">
+        <Link href="https://github.com/marcbrigham">
+          <Image
+            src="/github.svg"
+            width={30}
+            height={30}
+            alt="github"
+            priority
+            className="social-icon dark:invert"
+          />
+        </Link>
+        <Link href="https://x.com/marcbrigham">
+          <Image
+            src="/x.svg"
+            width={30}
+            height={30}
+            alt="github"
+            priority
+            className="fill-white dark:invert"
+          />
+        </Link>
+      </div>
+    </footer>
+  );
 }
